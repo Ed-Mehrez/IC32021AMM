f9b036485da455c69f7f4244f9e056f9e76ff8b3
Daxi <77559744+darciusal16@users.noreply.github.com>
2021-07-31 19:09:00 -0400

    Update truffle-config.js

--- onchain/truffle-config.js
+++ onchain/truffle-config.js
@@ -1,13 +1,3 @@
-let HDWalletProvider = require("truffle-hdwallet-provider");
-
-API_URL = "https://arb-rinkeby.g.alchemy.com/v2/P1JnmDbHqVDPLjTVu8UeA9AgHM4gXgpY";
-API_URL2 = "https://arbitrum-rinkeby.infura.io/v3/0be985387ace4f62b0b0b7c5487a3853";
-POLYGON_URL = "https://polygon-mumbai.g.alchemy.com/v2/epOefEZS5JxpbMbFaD7GYSxteAlPl4W-"
-PRIVATE_KEY = "4d76a300a44aeda851f474b8bf664c72d4cb180acc60778ff2c84aceb6e7db65";
-RINKEBY_URL = "https://eth-rinkeby.alchemyapi.io/v2/0B4lkTIdvHkAZuHgrbkpDHu98FySj8nc"
-MNEMONIC = "saddle what reveal island shoe strategy liar solid play impact suffer type";
-MNEMONIC2 = "uncle rather arctic fetch sleep ensure very silver consider clown device trend";
-
 module.exports = {
   // Uncommenting the defaults below
   // provides for an easier quick-start with Ganache.
@@ -25,13 +15,6 @@ module.exports = {
       host: "3.239.167.241",
       port: 8545,
       network_id: "*"
-    },
-    rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(MNEMONIC,RINKEBY_URL)
-      },
-      network_id: "*",
-      gas:29900000,      //make sure this gas allocation isn't over 4M, which is the max
     },
   },
   compilers: {
