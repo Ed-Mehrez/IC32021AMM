fee70793ec639a34cd9b5b692e4de79415977845
PatrickKiefer <39345464+PatrickKiefer@users.noreply.github.com>
2021-07-31 09:57:55 -0400

    more detail to IC3test.js

--- IC3tests.js
+++ IC3tests.js
@@ -1,24 +1,42 @@
 const axios = require('axios')
 
-const HedgingContract = require("../build/contracts/Reserves.json")
+const HedgingContract = require("./onchain/build/contracts/Reserve.json")
+const Router = require("./onchain/build/contracts/Router.json")
 const Web3 = require('web3')
 
 // connect to network
 const url = '3.239.167.241'
 const port = 8545
-const web3 = new Web3(`http://${url}:${port}/`)
+const web3 = new Web3(`ws://${url}:${port}/`) // https? not connecting
 
 // connect to web3 provider, use default account for signing
+async function init() {
+    console.log("init is running")
+    const networkId =  await web3.eth.net.getId() 
 
-// const new Web3.HttpProvider()
+    // const new Web3.HttpProvider()
 
-// instantiate local versions of hedging contract
+    // instantiate local versions of hedging contract
 
-const HCdeployedNetwork = HedgingContract.networks[networkId]
-const hedgingcontract = new web3.eth.Contract(
-    HedgingContract.abi,
-    HCdeployedNetwork.address
-)
+    const HCDeployedNetwork = HedgingContract.networks[networkId]
+    const hedgingcontract = new web3.eth.Contract(
+        HedgingContract.abi,
+        HCDeployedNetwork.address
+    )
+
+    console.log(['hedging contract address:', hedgingcontract])
+
+    const RouterDeployedNetwork = Router.networks[networkId]
+    const router = new web3.eth.Contract(
+        Router.abi,
+        RouterDeployedNetwork.address
+    )
+
+    console.log(['router address:', RouterDeployedNetwork.address])
+
+}
+
+init()
 
 // simulate sequence of API calls // call API with price request (and just assume user accepts any price) (can also send user address)
 async function getOptionPrice(expiration, currStockPrice, strike, sigma) {
